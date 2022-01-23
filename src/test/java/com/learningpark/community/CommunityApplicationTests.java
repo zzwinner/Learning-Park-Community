cde8fb559801b8ab5eaaec53ce1c6a9c5b8cd8be
zhouhao <1323819870@qq.com>
2022-01-23 16:17:51 +0800

    2022/1/23提交

--- src/test/java/com/learningpark/community/CommunityApplicationTests.java
+++ src/test/java/com/learningpark/community/CommunityApplicationTests.java
@@ -2,9 +2,13 @@ package com.learningpark.community;
 
 import com.learningpark.community.dao.UserMapper;
 import com.learningpark.community.entity.User;
+import com.learningpark.community.util.MailSendUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
 
 @SpringBootTest
 class CommunityApplicationTests {
@@ -15,6 +19,12 @@ class CommunityApplicationTests {
 
 	@Autowired
 	private UserMapper userMapper;
+
+	@Autowired
+	private MailSendUtil mailSendUtil;
+
+	@Autowired
+	private TemplateEngine templateEngine;
 
 	@Test
 	public void testSelectUser() {
@@ -28,4 +38,19 @@ class CommunityApplicationTests {
 		System.out.println(user);
 	}
 
+	@Test
+	public void testTextMail(){
+		mailSendUtil.sendMail("zuzhibugongshi17@163.com","测试","Hello Mail.");
+	}
+
+	@Test
+	public void testHtmlMail() {
+		Context context = new Context();
+		context.setVariable("username", "sunday");
+
+		String content = templateEngine.process("/mail/demo", context);
+		System.out.println(content);
+
+		mailSendUtil.sendMail("zuzhibugongshi17@163.com", "HTML", content);
+	}
 }
