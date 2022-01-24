ab763a454bccff069c0ef7e5b109d16010b67d92
zhouhao <1323819870@qq.com>
2022-01-24 16:44:35 +0800

    2022/1/24提交

--- src/main/java/com/learningpark/community/controller/LoginController.java
+++ src/main/java/com/learningpark/community/controller/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
-
+    //测试
     @RequestMapping(path = "/register",method = RequestMethod.GET)
     public String getRegisterPage(){
         return "/site/register";
