28d8f2725404f2fa43866eaa7806500d1b251b80
zhouhao <1323819870@qq.com>
2022-01-26 14:08:21 +0800

    2022/1/26提交

--- src/main/java/com/learningpark/community/controller/LoginController.java
+++ src/main/java/com/learningpark/community/controller/LoginController.java
@@ -1,8 +1,11 @@
 package com.learningpark.community.controller;
 
+import com.google.code.kaptcha.Producer;
 import com.learningpark.community.entity.User;
 import com.learningpark.community.service.UserService;
 import com.learningpark.community.util.CommunityConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,34 +13,46 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 @Controller
 public class LoginController implements CommunityConstant {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     private UserService userService;
 
-    @RequestMapping(path = "/register",method = RequestMethod.GET)
-    public String getRegisterPage(){
+    @Autowired
+    private Producer kaptchaProducer;
+
+    @RequestMapping(path = "/register", method = RequestMethod.GET)
+    public String getRegisterPage() {
         return "/site/register";
     }
 
-    @RequestMapping(path = "/login",method = RequestMethod.GET)
-    public String getLoginPage(){
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
+    public String getLoginPage() {
         return "/site/login";
     }
 
-    @RequestMapping(path = "/register",method = RequestMethod.POST)
-    public String register(Model model, User user){
-        Map<String,Object> map = userService.register(user);
-        if(map == null || map.isEmpty()){
-            model.addAttribute("msg","注册成功！我们已向您的邮箱发送一封激活邮件,请尽快激活！");
-            model.addAttribute("target","/index");
+    @RequestMapping(path = "/register", method = RequestMethod.POST)
+    public String register(Model model, User user) {
+        Map<String, Object> map = userService.register(user);
+        if (map == null || map.isEmpty()) {
+            model.addAttribute("msg", "注册成功！我们已向您的邮箱发送一封激活邮件,请尽快激活！");
+            model.addAttribute("target", "/index");
             return "/site/operate-result";
-        }else {
-            model.addAttribute("usernameMsg",map.get("usernameMsg"));
-            model.addAttribute("passwordMsg",map.get("passwordMsg"));
-            model.addAttribute("emailMsg",map.get("emailMsg"));
+        } else {
+            model.addAttribute("usernameMsg", map.get("usernameMsg"));
+            model.addAttribute("passwordMsg", map.get("passwordMsg"));
+            model.addAttribute("emailMsg", map.get("emailMsg"));
             return "/site/register";
         }
 
@@ -57,5 +72,25 @@ public class LoginController implements CommunityConstant {
             model.addAttribute("target", "/index");
         }
         return "/site/operate-result";
+    }
+
+    @RequestMapping(path = "/kaptcha", method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response, HttpSession session) {
+
+        //生成验证码
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
+
+        //将验证码(文字)传给session
+        session.setAttribute("kaptcha", text);
+
+        //将验证码(图片)传给浏览器
+        try {
+            OutputStream os = response.getOutputStream();
+            ImageIO.write(image, "png", os);
+        } catch (IOException e) {
+            LOGGER.error("相应验证码失败:" + e.getMessage());
+        }
+
     }
 }
